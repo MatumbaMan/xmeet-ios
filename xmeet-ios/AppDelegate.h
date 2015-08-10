c0bd3676dad024600a72326cf1f9951404cc8826
houkinglong <houkinglong@aliyun.com>
2015-08-10 20:33:27 +0800

    init

---
+++ xmeet-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xmeet-ios
+//
+//  Created by Kinglong on 15/8/10.
+//  Copyright (c) 2015年 Xmeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
